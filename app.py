2750d83ed35cb34cd02588cea3662e43c780ec45
mauroxcf <maurox03@gmail.com>
2022-09-05 01:44:39 -0500

    setting enviroment variables config

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flaskr.routes.personal_management import personal_management
+from config import DATABASE_CONNECTION_URI
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://root:root@localhost:3306/personaldb"
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 SQLAlchemy(app)
 app.register_blueprint(personal_management)
