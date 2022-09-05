539c59bdf7a1a29235f0cfa868859057ebad67c2
mauroxcf <maurox03@gmail.com>
2022-09-04 19:55:33 -0500

    fix post method and adding documentation

--- flaskr/models.py
+++ flaskr/models.py
@@ -9,7 +9,15 @@ class DocumentType(enum.Enum):
     cedula = "Cedula"
     pasaporte = "Pasaporte"
 
-class PersonalData(db.Model):
+class Basejson:
+    """ Class to have data in json without a specific parameter """
+    def to_dict(self):
+        """ Method to convert into dictionary """
+        dictionary: dict = self.__dict__
+        dictionary.pop('_sa_instance_state')
+        return dictionary
+
+class PersonalData(Basejson, db.Model):
     """Information of the Person
     """
     __tablename__ = 'personaldata'
--- flaskr/routes/personal_management.py
+++ flaskr/routes/personal_management.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flaskr.utils.db import db
-from flaskr.utils.helper import create_dict
-from flaskr.models import PersonalData
+from flaskr.models import PersonalData, DocumentType
 import json
 
 personal_management = Blueprint('personal Management', __name__)
@@ -12,46 +11,43 @@ def hello_world():
 
 @personal_management.route("/all", methods=['GET'])
 def get_all_person():
+    """
+    It creates a dictionary, queries the database for all persons, and then returns a json response
+    :return: A tuple of a jsonified dictionary and a status code.
+    """
     try:
-        mydict = create_dict()
         persons = PersonalData.query.all()
 
-        """ for row in persons:
-            mydict.add(row[0],({"document_type":row[1],"document_id":row[2],"phone":row[3],"last_name":row[4]})) """
-
-        #json_dump = json.dumps(persons)
-        #json_dump = json.dumps(mydict, indent=2, sort_keys=True)
-        #print(json_dump)
         return jsonify({"status": "success",
                             "message": "Successfully return all persons"}), 200
     except Exception as error:
         return jsonify({"status": "error",
-                        "message": "bad request",
-                        "data": "fixing"}), 400
+                        "message": "bad request"}), 400
 
 
 @personal_management.route("/new", methods=['POST'])
 def add_person():
+    """
+    It receives a JSON object, creates a new Person object, and saves it into the database
+    :return: a json object with the status and message.
+    """
     if request.method == 'POST':
         try:
             # receive data
             response: dict = request.get_json()
-            document_type = response.get("document_type")
+            document_type: DocumentType  = response.get("document_type")
             document_id: int = response.get("document_id")
             first_name: str = response.get("first_name")
-            lastname: str = response.get("last_name")
+            last_name: str = response.get("last_name")
             print(response)
             # create a new Person object
             new_person = PersonalData(document_type, document_id, first_name, last_name)
-            #json_dump = json.dumps(new_person)
-
             # save the object into the database
             db.session.add(new_person)
             db.session.commit()
 
             return jsonify({"status": "success",
-                            "message": "Successfully created person",
-                            "data": "fixing"}), 200
+                            "message": "Successfully created person"}), 200
 
         except Exception as error:
             return jsonify({"status": "error",
