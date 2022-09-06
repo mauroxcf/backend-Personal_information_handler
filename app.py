e18250bdd80f08cea7102ab30867f65f5aa57ec8
mauroxcf <maurox03@gmail.com>
2022-09-06 00:13:28 -0500

    adding CORS

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flaskr.routes.personal_management import personal_management
 from config import DATABASE_CONNECTION_URI
 from flask_migrate import Migrate
 from flaskr.utils.db import db
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 SQLAlchemy(app)
